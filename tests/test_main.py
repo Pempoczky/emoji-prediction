4af8c45b0c2e720b4ec462be96511bc494202dc1
Jules Roboz <r.julilla@gmail.com>
2023-11-20 10:38:35 +0100

    data exploration test

--- tests/test_main.py
+++ tests/test_main.py
@@ -1,11 +1,26 @@
-import unittest
-from main import hello_world
+# import unittest
+# from main import hello_world
+#
+#
+# class MainTest(unittest.TestCase):
+#     def test_hello(self):
+#         self.assertEqual(hello_world(), "Hello, World!")
+#
+#
+# if __name__ == '__main__':
+#     unittest.main()
+import matplotlib
+from collections import Counter
 
 
-class MainTest(unittest.TestCase):
-    def test_hello(self):
-        self.assertEqual(hello_world(), "Hello, World!")
+mapping = open('data/mapping.txt', encoding="utf-8").read()
+test_labels = open('data/test_labels.txt', encoding="utf-8").read()
+train_labels = open('data/train_labels.txt', encoding="utf-8").read()
+val_labels = open('data/val_labels.txt', encoding="utf-8").read()
+val_text = open('data/val_text.txt', encoding="utf-8").read()
+train_text = open('data/train_text.txt', encoding="utf-8").read()
+test_text = open('data/test_text.txt', encoding="utf-8").read()
+print(mapping)
 
-
-if __name__ == '__main__':
-    unittest.main()
+for word, count in Counter(train_text).most_common(10):
+    print(word, count)
