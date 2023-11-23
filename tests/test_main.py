9ce371ab635a4d5aad078f1b5c124a38e5008b62
Jules Roboz <r.julilla@gmail.com>
2023-11-23 16:38:50 +0100

    Document-term matrix done

--- tests/test_main.py
+++ tests/test_main.py
@@ -11,6 +11,8 @@
 #     unittest.main()
 import matplotlib.pyplot as plt
 import numpy as np
+from nltk.tokenize import LineTokenizer, TweetTokenizer
+from sklearn.feature_extraction.text import TfidfVectorizer
 
 
 mapping = open('data/mapping.txt', encoding="utf-8").read()
@@ -21,6 +23,9 @@ val_text = open('data/val_text.txt', encoding="utf-8").read()
 train_text = open('data/train_text.txt', encoding="utf-8").read()
 test_text = open('data/test_text.txt', encoding="utf-8").read()
 print(mapping)
+lt = LineTokenizer()
+tt = TweetTokenizer()
+vectorizer = TfidfVectorizer()
 val_labels_split = val_labels.split("\n")
 train_labels_split = train_labels.split("\n")
 data_length = len(val_labels_split) + len(train_labels_split)
@@ -32,11 +37,33 @@ for i in range(20):
     counts = np.append(counts, val_labels_split.count(str(i)) + train_labels_split.count(str(i)))
     percentages = np.append(percentages, 100 * (val_labels_split.count(str(i)) + train_labels_split.count(str(i))) / data_length)
 
-print(classes)
-print(counts)
-print(percentages)
-plt.xticks(classes)
-plt.bar(classes, counts)
-plt.xlabel("Class")
-plt.ylabel("Occurrence of class")
-plt.show()
+# print(len(tokentext))
+# print(len(train_labels_split))
+
+#split the data into datapoints by separating them by line
+tokentext = lt.tokenize(train_text)
+
+
+
+#tokenize the individual datapoints
+# for line, i in zip(tokentext, range(len(tokentext))):
+#     tokentext[i] = tt.tokenize(tokentext[i])
+#X here is the document-term matrix
+
+#applying PCA on this?
+#https://stackoverflow.com/questions/59162130/how-can-i-use-the-pca-for-a-term-document-matrix-in-python
+X = vectorizer.fit_transform(tokentext)
+print(vectorizer.get_feature_names_out())
+print(X.shape)
+print(X[0][0])
+#print(tokentext[0:6])
+# print(classes)
+# print(counts)
+# print(percentages)
+# plt.xticks(classes)
+# plt.bar(classes, counts)
+# plt.xlabel("Class")
+# plt.ylabel("Occurrence of class")
+# plt.show()
+
+train_text
