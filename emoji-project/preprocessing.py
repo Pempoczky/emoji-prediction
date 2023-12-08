98983fef35432dad2f4c01ae210f4379b8643ac2
Jules Roboz <r.julilla@gmail.com>
2023-12-08 17:21:59 +0100

    final cleanup

--- emoji-project/preprocessing.py
+++ emoji-project/preprocessing.py
@@ -1,14 +1,10 @@
 import numpy as np
 #from nltk.tokenize import LineTokenizer, TweetTokenizer
-from sklearn.feature_extraction.text import TfidfVectorizer
+from sklearn.feature_extraction.text import TfidfVectorizer, CountVectorizer
 from nltk.tokenize import LineTokenizer, TweetTokenizer
-from tempfile import TemporaryFile
 
-outfile = TemporaryFile()
-
-
-mapping = open('data/mapping.txt', encoding="utf-8").read()
-train_labels = open('data/train_labels.txt', encoding="utf-8").read()
+# mapping = open('data/mapping.txt', encoding="utf-8").read()
+# train_labels = open('data/train_labels.txt', encoding="utf-8").read()
 train_text = open('data/train_text.txt', encoding="utf-8").read()
 lt = LineTokenizer()
 tt = TweetTokenizer()
@@ -17,8 +13,9 @@ def tokenize(text):
     return tt.tokenize(text)
 
 vectorizer = TfidfVectorizer(tokenizer=tokenize, stop_words='english', token_pattern=None)
-tokentext = lt.tokenize(train_text)
+lineSeparatedText = lt.tokenize(train_text)
+X = vectorizer.fit_transform(lineSeparatedText)
+# print(vectorizer.get_feature_names_out()[30500:30600])
 
-X = vectorizer.fit_transform(tokentext)
 #save the array of features as a binary file in the features folder
 np.save('features/feature_data.npy', X)
